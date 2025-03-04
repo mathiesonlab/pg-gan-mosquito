6b9fa78b0c8921cf7fd6a3893550b3b5c486bcb8
Sara Mathieson <smathieson@smathieson-24.local>
2025-03-04 14:24:18 -0500

    reducesum

--- discriminator.py
+++ discriminator.py
@@ -12,6 +12,11 @@ from tensorflow.keras.layers import Dense, Flatten, Conv1D, Conv2D, \
 from tensorflow.keras import Model
 from scipy.special import rel_entr
 
+class ReduceSum(Layer):
+
+    def call(self, x):
+        return tf.math.reduce_sum(x, axis=1)
+
 
 class OnePopModel(Model):
     """Single population model - based on defiNETti software."""
@@ -44,7 +49,7 @@ class OnePopModel(Model):
         # note axis is 1 b/c first axis is batch
         # can try max or sum as the permutation-invariant function
         #x = tf.math.reduce_max(x, axis=1)
-        x = tf.math.reduce_sum(x, axis=1)
+        x = ReduceSum()(x)
 
         x = self.flatten(x)
         x = self.fc1(x)
@@ -113,8 +118,11 @@ class TwoPopModel(Model):
         # can try max or sum as the permutation-invariant function
         #x_pop1_max = tf.math.reduce_max(x_pop1, axis=1)
         #x_pop2_max = tf.math.reduce_max(x_pop2, axis=1)
-        x_pop1_sum = tf.math.reduce_sum(x_pop1, axis=1)
-        x_pop2_sum = tf.math.reduce_sum(x_pop2, axis=1)
+        #x_pop1_sum = tf.math.reduce_sum(x_pop1, axis=1)
+        #x_pop2_sum = tf.math.reduce_sum(x_pop2, axis=1)
+
+        x_pop1_sum = ReduceSum()(x_pop1)
+        x_pop2_sum = ReduceSum()(x_pop2)
 
         # flatten all
         #x_pop1_max = self.flatten(x_pop1_max)
