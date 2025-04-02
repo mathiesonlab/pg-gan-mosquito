8ce498b479ecab373b97e53fc2ae553b0e91dbdc
Sara Mathieson <smathieson@smathieson-24.local>
2025-04-02 10:35:09 -0400

    streamline ss

--- job_scripts/run_ss_multi.sh
+++ job_scripts/run_ss_multi.sh
@@ -5,21 +5,30 @@
 #$ -cwd
 #$ -j y
 
-ml anaconda3
-conda activate pg_gan_util
+#ml anaconda3
+#conda activate pg_gan_util
 
+DEMO=$1 # i.e. dadi_joint or dadi_joint_mig
 
-DATA=nsg
-DEMO=dadi_joint
-POP1=GN # CM
-POP2=BF # UG
-POP1S=88 # 594
-POP2S=334 # 224
-SPECIES=gam
-LR=25e-6 #1e-6
-DROPOUT=0.8
-INPUT=./sim_out/${POP1}-${POP2}_${SPECIES}_${DATA}/${DEMO}/sa/${LR}/${DROPOUT}/output.out
-OUTPUT=./sim_out/${POP1}-${POP2}_${SPECIES}_${DATA}/${DEMO}/sa/${LR}/${DROPOUT}/${POP1}-${POP2}_${SPECIES}_${DATA}_results_multi.png
+#DATA=nsg
+#DEMO=dadi_joint
+#POP1=GN # CM
+#POP2=BF # UG
+#POP1S=88 # 594
+#POP2S=334 # 224
+#SPECIES=gam
+#LR=25e-6 #1e-6
+#DROPOUT=0.8
+#INPUT=./sim_out/${POP1}-${POP2}_${SPECIES}_${DATA}/${DEMO}/sa/${LR}/${DROPOUT}/output.out
+#OUTPUT=./sim_out/${POP1}-${POP2}_${SPECIES}_${DATA}/${DEMO}/sa/${LR}/${DROPOUT}/${POP1}-${POP2}_${SPECIES}_${DATA}_results_multi.png
 
-echo "python summary_stats_multi.py ${INPUT} ${OUTPUT} ${DEMO}"
-#python summary_stats_multi.py ${INPUT} ${OUTPUT} ${DEMO}
+for SEED in 0 1 2 3 4
+do
+    # summary stats
+    echo "python3 /homes/smathieson/GIT/pg-gan-mosquito/summary_stats_multi.py output${SEED}.txt output${SEED}.pdf ${DEMO}"
+    #python3 /homes/smathieson/GIT/pg-gan-mosquito/summary_stats_multi.py output${SEED}.txt output${SEED}.pdf ${DEMO}
+
+    # loss plot
+    echo "python3 /homes/smathieson/GIT/pg-gan-mosquito/plot_loss.py -i output${SEED}.txt -o output${SEED}_loss.pdf"
+    #python3 /homes/smathieson/GIT/pg-gan-mosquito/plot_loss.py -i output${SEED}.txt -o output${SEED}_loss.pdf
+done
