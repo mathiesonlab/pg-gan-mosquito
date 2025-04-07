94b1ef3e8b3467ad1c1a2953159b9a5b13cd3b5b
Sara Mathieson <sara.k.mathieson@gmail.com>
2025-04-07 13:53:34 -0400

    start model selection

--- job_scripts/run_demo_sel.sh
+++ job_scripts/run_demo_sel.sh
@@ -8,11 +8,11 @@
 INPUT=CM-UG_gam_nsg.h5
 OUTPUT_PREFIX=./sim_out/model_compare/CM-UG_gam
 OUTPUT=${OUTPUT_PREFIX}/model_comparison.out
-CPROF=${OUTPUT_PREFIX}/cprof.txt
+#CPROF=${OUTPUT_PREFIX}/cprof.txt
 
 source ./tfenv_2.13/bin/activate
-echo "dadi_joint_mig vs dadi_joint_mig posterior, 2 pool layer, fc1 2 = 160 128, testing" > ${OUTPUT_PREFIX}/README
-qstat | tail -n 1 | awk '{print $1}' >> ${OUTPUT_PREFIX}/README
-python3 -m cProfile -o ${CPROF} demographic_selection_oop.py -d ${INPUT} -b ${OUTPUT_PREFIX} -s -t > ${OUTPUT}
-echo "job completed" >> ${OUTPUT_PREFIX}/README
+echo "dadi_joint_mig vs dadi_joint_mig posterior, 2 pool layer, fc1 2 = 160 128, testing"
+#qstat | tail -n 1 | awk '{print $1}' >> ${OUTPUT_PREFIX}/README
+python3 demographic_selection_oop.py -d ${INPUT} -b ${OUTPUT_PREFIX} -s -t > ${OUTPUT}
+#echo "job completed" >> ${OUTPUT_PREFIX}/README
 
