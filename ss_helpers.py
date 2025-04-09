5ef2ad85d53302cd5bd65f3cd955e4a4e9df0f72
Sara Mathieson <smathieson@smathieson-24.local>
2025-04-09 13:50:16 -0400

    intersnp

--- ss_helpers.py
+++ ss_helpers.py
@@ -530,6 +530,9 @@ def stats_all(matrices, matrices_region, L = global_vars.L):
             pop_sfs[s].append(sfs[s])
 
         # inter-snp (first is 0 for real data so clip)
+        to_save = [round(x*L) for x in intersnp]
+        print(",".join([str(p) for p in to_save]))
+        input('enter')
         pop_dist.extend([round(x*L) for x in intersnp[1:]])
 
         # LD
