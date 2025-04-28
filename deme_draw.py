38aff42f1928adcc995a2d89d1157722ee905087
Sara Mathieson <sara.k.mathieson@gmail.com>
2025-04-28 15:30:44 -0400

    deme draw

--- deme_draw.py
+++ deme_draw.py
@@ -14,10 +14,13 @@ import matplotlib.pyplot as plt
 import argparse
 import os
 
+POP1 = "GN"
+POP2 = "BF"
+
 def main():
     # Set up argument parsing
     parser = argparse.ArgumentParser(description="Draw demographic history plots")
-    parser.add_argument("model", choices=["mig", "no_mig"], help="Demographic model to use: 'mig' or 'no_mig'")
+    parser.add_argument("--model", choices=["mig", "no_mig"], help="Demographic model to use: 'mig' or 'no_mig'")
     
     parser.add_argument("--NI", type=float, required=True, help="Initial ancestral population size")
     parser.add_argument("--TG", type=float, required=True, help="Time when the ancestral population begins to change in size (generations)")
@@ -45,18 +48,18 @@ def main():
         g = -(1/(TG-TS)) * math.log(args.NI/args.NF)
 
         demography = msprime.Demography()
-        demography.add_population(name="POP1", initial_size=args.NF1)
-        demography.add_population(name="POP2", initial_size=args.NF2)
+        demography.add_population(name=POP1, initial_size=args.NF1)
+        demography.add_population(name=POP2, initial_size=args.NF2)
         demography.add_population(name="ANC", initial_size=args.NF, initially_active=False)
 
-        demography.add_population_parameters_change(time=0, growth_rate=g1, population="POP1")
-        demography.add_population_parameters_change(time=0, growth_rate=g2, population="POP2")
+        demography.add_population_parameters_change(time=0, growth_rate=g1, population=POP1)
+        demography.add_population_parameters_change(time=0, growth_rate=g2, population=POP2)
         
         if args.model == "mig":
             MG = args.MG / 2 / args.NF
-            demography.set_symmetric_migration_rate(populations=["POP1", "POP2"], rate=MG)
+            demography.set_symmetric_migration_rate(populations=[POP1, POP2], rate=MG)
         
-        demography.add_population_split(time=TS, derived=["POP1", "POP2"], ancestral="ANC")
+        demography.add_population_split(time=TS, derived=[POP1, POP2], ancestral="ANC")
         demography.add_population_parameters_change(time=TS, growth_rate=g, population="ANC")
         demography.add_population_parameters_change(time=TG, growth_rate=0, population="ANC")
         
@@ -70,14 +73,14 @@ def main():
             graph,
             ax=ax,
             log_time=log_time,
-            title=f"dadi_joint_posterior_{args.model}"
+            title=f"dadi_joint_{args.model}_posterior"
         )
 
         # Create output directory if it doesn't exist
         if not os.path.exists(args.out_dir):
             os.makedirs(args.out_dir)
         
-        plt.savefig(os.path.join(args.out_dir, f"dadi_joint_posterior_{args.model}.png"))
+        plt.savefig(os.path.join(args.out_dir, f"dadi_joint_posterior_{args.model}.pdf"))
         demes.dump(graph, os.path.join(args.out_dir, f"dadi_joint_posterior_{args.model}.yaml"))
         
         print(f"Demographic history plot and YAML file for model '{args.model}' saved successfully in '{args.out_dir}'.")
--- job_scripts/run_deme_draw.sh
+++ job_scripts/run_deme_draw.sh
@@ -5,8 +5,35 @@
 #$ -cwd
 #$ -j y
 
+# reduce_mean_filter_param3, dadi_joint, output2
+MODEL=no_mig
+NI=579516
+TG=75249
+NF=2784452
+TS=3519
+NI1=117287221
+NI2=62771288
+NF1=59092640
+NF2=189066655
+OUTDIR=/Users/saramathieson/Dropbox/pg-gan/jacky/redo_results/demes/$MODEL
 
-ml anaconda3
-conda activate /data/home/bty414/.conda/envs/pg_gan_util
+echo "python3 deme_draw.py --model $MODEL --NI $NI --TG $TG --NF $NF --TS $TS --NI1 $NI1 --NI2 $NI2 --NF1 $NF1 --NF2 $NF2 --out_dir $OUTDIR"
+python3 deme_draw.py --model $MODEL --NI $NI --TG $TG --NF $NF --TS $TS --NI1 $NI1 --NI2 $NI2 --NF1 $NF1 --NF2 $NF2 --out_dir $OUTDIR
 
-python deme_draw.py
+# reduce_mean_filter_param3, dadi_joint_mig, output1
+MODEL=mig
+NI=560796
+TG=57254
+NF=7772061
+TS=2265
+NI1=16137900
+NI2=20976274
+NF1=134237900
+NF2=218082996
+MG=27
+OUTDIR=/Users/saramathieson/Dropbox/pg-gan/jacky/redo_results/demes/$MODEL
+
+echo "python3 deme_draw.py --model $MODEL --NI $NI --TG $TG --NF $NF --TS $TS --NI1 $NI1 --NI2 $NI2 --NF1 $NF1 --NF2 $NF2 --MG $MG --out_dir $OUTDIR"
+python3 deme_draw.py --model $MODEL --NI $NI --TG $TG --NF $NF --TS $TS --NI1 $NI1 --NI2 $NI2 --NF1 $NF1 --NF2 $NF2 --MG $MG --out_dir $OUTDIR
+
+
