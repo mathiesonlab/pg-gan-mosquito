559979b2571b607d35030d9d6c001d579188df45
Sara Mathieson <smathieson@smathieson-24.local>
2025-05-29 15:29:10 -0400

    redo loss plots

--- job_scripts/run_ss_multi.sh
+++ job_scripts/run_ss_multi.sh
@@ -6,8 +6,8 @@ FOLDER=$2 # code folder
 for SEED in 0 1 2 3 4
 do
     # summary stats
-    echo "python3 $2/summary_stats_multi.py output${SEED}.txt output${SEED}.pdf ${DEMO}"
-    python3 $2/summary_stats_multi.py output${SEED}.txt output${SEED}.pdf ${DEMO}
+    #echo "python3 $2/summary_stats_multi.py output${SEED}.txt output${SEED}.pdf ${DEMO}"
+    #python3 $2/summary_stats_multi.py output${SEED}.txt output${SEED}.pdf ${DEMO}
 
     # loss plot
     echo "python3 $2/plotting/plot_loss.py -i output${SEED}.txt -o output${SEED}_loss.pdf"
