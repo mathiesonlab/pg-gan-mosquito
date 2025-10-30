553bc68dc54cd24fff33dacd2663194435f48b75
Sara Mathieson <smathieson@smathieson-24.local>
2025-10-30 06:10:08 -0400

    ss multi

--- job_scripts/run_ss_multi.sh
+++ job_scripts/run_ss_multi.sh
@@ -2,14 +2,15 @@
 
 DEMO=$1 # i.e. dadi_joint or dadi_joint_mig
 FOLDER=$2 # code folder
+PREFIX=$3 # i.e. CI_MZ_dadi_joint
 
-for SEED in 0 1 2 3 4
+for SEED in 0 1
 do
     # summary stats
     #echo "python3 $2/summary_stats_multi.py output${SEED}.txt output${SEED}.pdf ${DEMO}"
     #python3 $2/summary_stats_multi.py output${SEED}.txt output${SEED}.pdf ${DEMO}
 
     # loss plot
-    echo "python3 $2/plotting/plot_loss.py -i output${SEED}.txt -o output${SEED}_loss.pdf"
-    python3 $2/plotting/plot_loss.py -i output${SEED}.txt -o output${SEED}_loss.pdf
+    echo "python3 $2/plotting/plot_loss.py -i ${PREFIX}${SEED}.txt -o ${PREFIX}${SEED}_loss.pdf"
+    python3 $2/plotting/plot_loss.py -i ${PREFIX}${SEED}.txt -o ${PREFIX}${SEED}_loss.pdf
 done
