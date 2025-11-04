f18f2478bfa04c2586b9c3049e135704f70e7e2f
Sara Mathieson <smathieson@smathieson-24.local>
2025-11-04 11:57:30 -0500

    update ss multi

--- job_scripts/run_ss_multi.sh
+++ job_scripts/run_ss_multi.sh
@@ -4,7 +4,7 @@ DEMO=$1 # i.e. dadi_joint or dadi_joint_mig
 FOLDER=$2 # code folder
 PREFIX=$3 # i.e. CI_MZ_dadi_joint
 
-for SEED in 3
+for SEED in 0 1 2 3 4
 do
     # summary stats
     echo "python3 $2/summary_stats_multi.py ${PREFIX}${SEED}.txt ${PREFIX}${SEED}.pdf ${DEMO}"
