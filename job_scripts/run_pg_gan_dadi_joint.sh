99ef8772dcee6c0d8d1ed4825fb67f6c177bec41
Sara Mathieson <smathieson@smathieson-24.local>
2026-01-27 15:51:58 -0500

    parcc script

---
+++ job_scripts/run_pg_gan_dadi_joint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for SEED in 0 1 2 3 4 5 6 7 8 9
+do
+    # no-mig
+    python3 /home/mathiesonlab-adm/GIT/pg-gan-mosquito/pg_gan.py -m dadi_joint -p NI,TG,NF,TS,NI1,NI2,NF1,NF2 -n 62,162 --pt_lr 1e-3 --pt_dropout 0.25 --phase full_training -s ${SEED}
+done
